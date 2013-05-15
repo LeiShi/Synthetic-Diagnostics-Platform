b5a36d8c8ee0aed4340dfb585bc334b3f45d9cb4
shilei8583 <shilei8583@gmail.com>
2013-05-15 14:05:05 +0000

    Add linear H mode profile

--- src/Plasma/TestParameter.py
+++ src/Plasma/TestParameter.py
@@ -11,11 +11,11 @@ from ..Geometry import Grid
 # Parameter is a keyword dictionary contains all the pre-defined profile parameters.
 # can be modified via method set_parameter(kwargs).
 # Note that all the coordinates are in (Z,R) order.
-Parameter = { 'DownLeft':(-60,200), 'UpRight':(60,260),'NR':100, 'NZ':200, 'ne_0': 1e12, 'Te_0': 10*cgs['keV'], 'B_0': 5000, 'ne_shape': 'exp', 'Te_shape': 'exp', 'a': 60, 'R_0': 200}
+Parameter = { 'DownLeft':(-60,200), 'UpRight':(60,260),'NR':100, 'NZ':200, 'ne_0': 1e12, 'Te_0': 10*cgs['keV'], 'B_0': 5000, 'ne_shape': 'Hmode', 'Te_shape': 'Hmode', 'a': 50, 'R_0': 200}
 
 # shape table is a dictionary contains the shape parameters
 # Do not suggest to change it by outside programs
-ShapeTable = {'exp': {'NeDecayScale': 3, 'TeDecayScale':5} }
+ShapeTable = {'exp': {'NeDecayScale': 3, 'TeDecayScale':5} , 'Hmode':{'PedWidthT': 0.2,'PedWidthN': 0.2 ,'PedHightT': 0.8, 'PedHightN': 0.7, 'ne_out': 1e-10, 'Te_out': 1e-10}}
 
 def show_parameter():
     """Print out the parameters at the moment
@@ -68,6 +68,14 @@ def create_profile():
         DecScale= ShapeTable['exp']['NeDecayScale']
         DecLength= a/DecScale
         ne_array= ne_0 * np.exp( -np.sqrt(((RZGrid.R2D-R_0)**2+RZGrid.Z2D**2))/DecLength )
+    elif (nshp == 'Hmode') :
+#linear H mode profile
+        nped = ne_0 * ShapeTable['Hmode']['PedHightN']
+        nout = ne_0 * ShapeTable['Hmode']['ne_out']
+        a_core = a * (1-ShapeTable['Hmode']['PedWidthN'])
+        a_array = np.sqrt(((RZGrid.R2D-R_0)**2+RZGrid.Z2D**2))
+# linear function connecting axis, top of pedestal and the vacuum
+        ne_array = np.select([ a_array <= a_core, a_array >= a, a_array > a_core ],[a_array* (nped - ne_0)/a_core + ne_0 , nout,  (a_array - a) * (nped-nout)/(a_core - a)+ nout])
     profile['ne']= ne_array
 
 #Te profile
@@ -77,6 +85,14 @@ def create_profile():
     if ( tshp == 'exp'):
         DecLength= a/ShapeTable['exp']['TeDecayScale']
         Te_array= Te_0 * np.exp( -np.sqrt(((RZGrid.R2D-R_0)**2+RZGrid.Z2D**2))/DecLength )
+    elif (tshp == 'Hmode') :
+#linear H mode profile
+        tped = Te_0 * ShapeTable['Hmode']['PedHightT']
+        tout = Te_0 * ShapeTable['Hmode']['Te_out']
+        a_core = a * (1-ShapeTable['Hmode']['PedWidthT'])
+        a_array = np.sqrt(((RZGrid.R2D-R_0)**2+RZGrid.Z2D**2))
+# linear function connecting axis, top of pedestal and the vacuum
+        Te_array = np.select([ a_array <= a_core, a_array >= a, a_array > a_core  ],[a_array* (tped - Te_0)/a_core + Te_0 , tout , (a_array - a) * (tped-tout)/(a_core - a)+ tout])
     profile['Te']= Te_array
 
 # B profile
