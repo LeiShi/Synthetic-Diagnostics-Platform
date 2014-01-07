06c2c8d82dfe1a39fc286c12840ddadd4b9a4b7a
shilei8583 <shilei8583@gmail.com>
2014-01-07 19:59:38 +0000

    remove Python_C_Modules

--- src/Python_C_Modules/test/helloworld.py
+++
@@ -1,2 +0,0 @@
-s = 'Hello World!'
-print s
--- src/Python_C_Modules/test/test.py
+++
@@ -1,9 +0,0 @@
-"""Test module for combined C & Python module setup
-"""
-from test_C import *
-
-def pyprint(s):
-    print 'printing from python:'
-    print s
-
-    
