364a91b3a8d769189f27162bd9fce4c32b3fb050
LHausammann <LHausammann@18d86fea-8736-4268-b236-ce869c394d86>
2015-03-11 18:28:58 +0000

    add comment and remove some print

--- src/FPSDP/Diagnostics/BES/beam.py
+++ src/FPSDP/Diagnostics/BES/beam.py
@@ -24,6 +24,82 @@ def to_other_index(pos):
 
 class Beam1D:
     """ Simulate a 1D beam with the help of datas from simulation
+
+        Methods:
+        __init__(config_file,timesteps,data)
+                            -- config_file is the name of the config file,
+                               timesteps is the list of the timesteps wanted
+                               and data are the data from XGC1 (for example)
+       
+        get_width(dist)     -- return the standard deviation at the distance given
+
+        create_mesh()       -- create the points of the mesh
+
+        find_wall(eps=1e-6) -- find the end (for the beam) of the mesh from the data
+                               eps is for avoiding the end of the mesh (relative length)
+
+        get_electron_density(pos,t_) 
+                            -- function simplificating the acces to the data
+                               (avoid the problem of the two representations)
+                               pos is a 1D or 2D array (first index is the dimension)
+                               the coordinate are the same than in the beam config file
+
+        get_ion_density(pos,t_)
+                            -- same as before
+                    
+        get_electron_temp(pos)
+                            -- same as before (the perturbation of the temperature)
+                               are not taken in account (small effect)
+
+        get_ion_temp(self,pos)
+                            -- same as before
+
+        compute_beam_on_mesh()
+                            -- compute the density of the beam on the mesh with a
+                               Gauss-Legendre formula (two points)
+ 
+        get_mesh()          -- return the mesh, same coordinate than the beam config file
+                               first index is for the dimension
+ 
+        get_origin()        -- return the origin of the beam
+        
+        get_beam_density(pos,t_)
+                            -- use a gaussian profile and an interpolation in order to
+                               compute the beam density at the position. t_ is the index
+                               for the time
+
+
+        get_emis(pos,t_)    -- compute the emissivity \epsilon 
+
+        Attribut:
+    
+        cfg_file     --  name of the config file
+        data         --  data from the simulation
+        timesteps    --  list of the timesteps considered
+        adas_atte    --  list of file for the attenuation
+        adas_emis    --  list of file for the emission
+        collisions   --  class containing all the informations for the collisions
+        coll_atte    --  tuple relating the adas file with the beams (choice of collision)
+                         for the attenuation
+        coll_emis    --  same as before but with the emission
+        mass_b       --  mass of the particles in the beam (in amu)
+        beam_comp    --  energy of each beam components
+        power        --  total power of the beam
+        frac         --  fraction of power for each components
+        pos          --  origin of the beam
+        direc        --  direction of the beam
+        beam_width   --  FWHM of the beam at the origin
+        Nz           --  number of point on the beam mesh
+
+        inters       --  ending point of the mesh
+        mesh         --  mesh (first index for the dimension)
+
+        density_beam --  beam density at the grid points
+        speed        --  speed of each particles in a beam component
+        std_dev      --  standard deviation of the beam at the origin
+        std_dev2     --  square of std_dev
+        dens         --  electron density at the grid points
+
     """
     
     def __init__(self,config_file,timesteps,data):
@@ -87,8 +163,10 @@ class Beam1D:
         # intersection between end of mesh and beam
         self.inters = self.find_wall()                                       #!
         length = np.sqrt(sum((self.pos-self.inters)**2))
+        # distance to the origin along the central line
         self.dl = np.linspace(0,length,self.Nz)
         self.mesh = np.zeros((3,self.Nz))                                    #!
+        # the first index corresponds to the dimension (X,Y,Z)
         self.mesh[0,:] = self.pos[0] + self.dl*self.direc[0]
         self.mesh[1,:] = self.pos[1] + self.dl*self.direc[1]
         self.mesh[2,:] = self.pos[2] + self.dl*self.direc[2]
@@ -126,6 +204,7 @@ class Beam1D:
             tz = float('Inf')
 
         t = min([tx,ty,tz])
+        # check if there is an intersection
         if t is float('Inf'):
             raise NameError('Cannot find the wall for the beam')
 
@@ -138,6 +217,7 @@ class Beam1D:
             pos  --  position (3D array) where to compute the density
         """
         a = to_other_index(pos)
+        # the order of the grid is due to the XGC loading coordinate
         return Fint.trilinear_interp_1pt(self.data.grid.Z1D,self.data.grid.Y1D,
                                          self.data.grid.X1D,
                                          self.data.ne_on_grid[0,t_,:,:,:],a)
@@ -161,19 +241,22 @@ class Beam1D:
         """
         self.density_beam = np.zeros((len(self.timesteps),
                                      len(self.beam_comp),self.Nz))           #!
+        # speed of each beam
         self.speed = np.zeros(len(self.beam_comp))                           #!
         self.speed = np.sqrt(2*self.beam_comp*9.6485383e10/self.mass_b)
+        # get the standard deviation at the origin
         self.std_dev = self.beam_width/(2*np.sqrt(2*np.log(2)))              #!
         self.std_dev2 = self.std_dev**2
+        # density of the beam at the origin
         n0 = np.zeros(len(self.beam_comp))
         n0 = np.sqrt(2*self.mass_b*1.660538921e-27)*math.pi*self.power
         n0 *= self.frac*self.std_dev2/(self.beam_comp*1.60217733e-16)**(1.5)
-        self.dens = np.zeros((len(self.timesteps),len(self.mesh[0,:])))
+        self.dens = np.zeros((len(self.timesteps),len(self.mesh[0,:])))      #!
 
         for t_ in range(len(self.timesteps)):
-            print t_
+            print 'Timestep number: ', t_, ' over ', len(self.timesteps)
             for j in range(len(self.mesh[0,:])):
-                # density over the central line
+                # density over the central line (usefull for some check)
                 self.dens[t_,j] = self.get_electron_density(self.mesh[:,j],t_)
                 if j is not 0:
                     temp_beam = np.zeros(len(self.beam_comp))
@@ -212,11 +295,12 @@ class Beam1D:
                         S2 = self.collisions.get_attenutation(
                             self.beam_comp[beam_nber],self.mass_b[beam_nber],
                             ni2,T2,file_nber)
-                        
+                        # half distance between a & b
                         norm_ = 0.5*np.sqrt(sum((b-a)**2))
                         temp1 = (ne1*S1 + ne2*S2)
                         temp1 *= norm_/self.speed[beam_nber]
                         temp_beam[beam_nber] += temp1
+
                     self.density_beam[t_,:,j] = self.density_beam[t_,:,j-1] - \
                                                 temp_beam
 
@@ -281,6 +365,8 @@ class Beam1D:
             dist[0,:] = pos[0,:] - self.get_origin()[0]
             dist[1,:] = pos[1,:] - self.get_origin()[1]
             dist[2,:] = pos[2,:] - self.get_origin()[2]
+            # result is a 1D array containing the projection of the distance
+            # from the origin over the direction of the beam
             proj = np.einsum('ij,i->j',dist,self.direc)
             stddev2 = self.get_width(proj)**2
             # before the beam, the density will be set to 0
@@ -288,7 +374,6 @@ class Beam1D:
             indin =  np.where((proj > 0) & (proj < self.dl[-1]))
             # cubic spline for finding the value along the axis
             for i in range(len(self.beam_comp)):
-                print self.density_beam.shape
                 tck = interpolate.splrep(self.dl,self.density_beam[t_,i,:])
                 for j in range(len(indin)):
                     nb[i,indin[j]] = interpolate.splev(proj[indin[j]],tck)
@@ -297,6 +382,7 @@ class Beam1D:
             R2[0,:] = dist[0,:] - proj*self.direc[0]
             R2[1,:] = dist[1,:] - proj*self.direc[1]
             R2[2,:] = dist[2,:] - proj*self.direc[2]
+            # compute the norm of each position
             R2 = np.einsum('ij,ij->j',R2,R2)
             for i in range(len(self.beam_comp)):
                 nb[i,:] = nb[i,:]*np.exp(-R2/(2*stddev2))
@@ -312,15 +398,19 @@ class Beam1D:
             pos   -- 2D array, first index is for X,Y,Z
         """
         print 'DO NOT TAKE LIFETIME EFFECT'
+        # first take all the value needed for the computation
         n_b = self.get_beam_density(pos,t_)
         n_e = self.get_electron_density(pos,t_)
         emis = np.zeros((len(self.beam_comp),len(pos[0,:])))
         Te = self.get_electron_temp(pos)
+        # loop over all the type of collisions
         for k in self.coll_atte:
             file_nber = k[0]
             beam_nber = k[1]
+            # compute the emission coefficient
             emis[beam_nber,:] += self.collisions.get_emission(
                 self.beam_comp[beam_nber],n_e,self.mass_b[beam_nber],Te,file_nber)
+        # compute the emissivity
         for i in range(len(self.beam_comp)):
             emis[i] *= n_e*n_b[i,:]
         return emis
