d9449f737e1116b157a7e731e386671b63fa61f0
LHausammann <LHausammann@18d86fea-8736-4268-b236-ce869c394d86>
2015-05-18 20:30:16 +0000

    tool

--- src/FPSDP/Diagnostics/BES/tools.py
+++ src/FPSDP/Diagnostics/BES/tools.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp2d
 from scipy.fftpack import fft2, ifft2
@@ -7,7 +8,16 @@ import FPSDP.Diagnostics.BES.bes as bes_
 
 class Tools:
     """ Defines a few tools for doing some computations on the BES image
+    
+    Two different methods of initalization have been made:
+    The first one is the usual __init__ function that take as input the photon radiance, the R,Z coordinates
+    and the psin value, and the second one read a npz file with a standard order and calls the __init__ method.
 
+    :param np.array[Nt,Nfib] I: Photon radiance
+    :param np.array[Nfib] R: R coordinate
+    :param np.array[Nfib] Z: Z coordinate
+    :param np.array[Nfib] psin: :math:`\Psi_n` value
+    :param bool I_fluc: Input are fluctuation or total intensity
     """
 
     def __init__(self,I,R,Z,psin,I_fluc=False):
@@ -24,10 +34,13 @@ class Tools:
         self.Nt = I.shape[0]
 
     @classmethod
-    def init_from_file(cls,filename):
+    def init_from_file(cls,filename,fluc=False):
         """ Load of file in the numpy format (npz)
 
         The data inside it should be in the following order: [I,psin,pos_foc,...]
+
+        :param str filename: Name of the file
+        :param bool fluc: The intensity inside the file is the total (False) or the fluctuation (True)
         """
         data = np.load(filename)
         I = data['arr_0']
@@ -35,10 +48,11 @@ class Tools:
         pos = data['arr_2']
         R = np.sqrt(np.sum(pos[:,0:2]**2,axis=1))
         Z = pos[:,2]
-        return cls(I,R,Z,psin)
+        return cls(I,R,Z,psin,fluc)
 
     def interpolate(self,Nr,Nz,I):
-        """ Interpolate all the data on a mesh
+        """ Interpolate all the data on a spatial mesh and create this mesh.
+        The interpolation is done for each timestep
         """
         if not hasattr(self,'r'):
             self.r = np.linspace(np.min(self.R),np.max(self.R),Nr)
@@ -53,7 +67,226 @@ class Tools:
             Igrid[i,:,:] = temp(self.r,self.z).T
             
         return Igrid
+
+    def fluctuations_picture(self,timestep,v=40):
+        """ Plot a graph of the fluctuation
+        :param int timestep: Index of the timestep
+        :param int v: Number of ticks for the colorbar
+        """
+        plt.figure()
+        plt.tricontourf(self.R,self.Z,self.I[timestep,:],v)
+        plt.colorbar()
+        plt.plot(self.R,self.Z,'x')
+        plt.tricontour(self.R,self.Z,self.psin,[1])
+        plt.xlabel('R[m]')
+        plt.ylabel('Z[m]')
+        plt.show()
+
+    def fluctuations_movie(self,v=40):
+        """ Make a movie from the data and save them in movie.mp4
+        :param int v: Number of ticks for the colorbar
+        """        
+        from matplotlib import animation
+        fig = plt.figure()
+        ax = plt.gca()
+
+        plt.xlabel('R[m]')
+        plt.ylabel('Z[m]')
+
+        v = np.linspace(np.min(self.I),np.max(self.I),v)
+        
+        def animate(i):
+            ax.cla()
+            print 'Timestep: ', i
+            plt.title('Timestep : {}'.format(i))
+            plt.tricontourf(self.R,self.Z,self.I[i,:],v)
+            plt.plot(self.R,self.Z,'x')
+            plt.tricontour(self.R,self.Z,self.psin,[1])
+            fig.canvas.draw()
+            return None
+
+        # call the animator.  blit=True means only re-draw the parts that have changed.
+        anim = animation.FuncAnimation(fig, animate, frames=self.I.shape[0],repeat=False)
+        
+        # save the animation as an mp4.  This requires ffmpeg or mencoder to be
+        # installed.  The extra_args ensure that the x264 codec is used, so that
+        # the video can be embedded in html5.  You may need to adjust this for
+        # your system: for more information, see
+        # http://matplotlib.sourceforge.net/api/animation_api.html
+
+        FFwriter = animation.FFMpegWriter()
+        anim.save('movie.mp4', writer=FFwriter,fps=15, extra_args=['-vcodec', 'libx264'])
+
+        
+    def comparison_picture(self,tool2,timestep,v=40):
+        """ Make a picture from the data of the fluctuation
+        :param Tools tool2: Second instance of the Tools class
+        :param int timestep: Index of the timestep
+        :param int v: Number of ticks for the colorbar
+        """        
+
+        fig, axarr = plt.subplots(1,2,sharey=True)
+
+        axarr[0].set_title('Density fluctuation')
+
+        axarr[1].set_title('Synthetic BES')
+        min_ = np.min([np.min(self.I[timestep,:]), np.min(tool2.I[timestep,:])])
+        max_ = np.max([np.max(self.I[timestep,:]), np.max(tool2.I[timestep,:])])
+        v = np.linspace(min_,max_,v)
+        axarr[0].plot(tool2.R,tool2.Z,'x')
+
+        tri1 = axarr[0].tricontourf(tool2.R,tool2.Z,tool2.I[timestep,:],v)
+        axarr[0].tricontour(self.R,self.Z,self.psin,[1])
+
+        axarr[0].set_xlabel('R[m]')
+        axarr[0].set_ylabel('Z[m]')
+        
+        tri2 = axarr[1].tricontourf(self.R,self.Z,self.I[timestep,:],v)
+        axarr[1].tricontour(self.R,self.Z,self.psin,[1])
+        axarr[1].set_xlabel('R[m]')
+        axarr[1].plot(self.R,self.Z,'x')
+        plt.colorbar(tri1)
+
+        plt.show()
+
+    def comparison_movie(self,tool2,v=40):
+        """ Make a movie from the data and save them in movie.mp4
+        :param int timestep: Index of the timestep
+        :param int v: Number of ticks for the colorbar
+        """        
+        from matplotlib import animation
+
+        v1 = np.linspace(np.min(self.I),np.max(self.I),v)
+        v2 = np.linspace(np.min(tool2.I),np.max(tool2.I),v)
+        fig, axarr = plt.subplots(1,2)
+
+        axarr[0].set_title('Density fluctuation')
+        axarr[1].set_title('Synthetic BES')
+        
+        axarr[0].set_xlabel('R[m]')
+        axarr[0].set_ylabel('Z[m]')
+        axarr[1].set_xlabel('R[m]')
+
+        
+        tri0 = axarr[0].tricontourf(self.R,self.Z,self.I[0,:],v1)
+        tri1 = axarr[1].tricontourf(tool2.R,tool2.Z,tool2.I[0,:],v2)
+
+        cb = plt.colorbar(tri0,ax=axarr[0])
+        cb = plt.colorbar(tri1,ax=axarr[1])
+
+
+        def animate(i):
+            print 'Timestep: ', i
+            axarr[0].cla()
+            axarr[0].locator_params(axis = 'x',nbins=5)
+            axarr[1].cla()
+            axarr[1].locator_params(axis = 'x',nbins=5)
+  
+            axarr[1].set_yticklabels([])
+            plt.title('Timestep : {}'.format(i))
             
+            axarr[0].plot(tool2.R,tool2.Z,'x')
+            tri1 = axarr[0].tricontourf(tool2.R,tool2.Z,tool2.I[i,:],v2)
+            axarr[0].tricontour(self.R,self.Z,self.psin,[1])
+
+            axarr[1].plot(self.R,self.Z,'x')
+            tri2 = axarr[1].tricontourf(self.R,self.Z,self.I[i,:],v1)
+            axarr[1].tricontour(self.R,self.Z,self.psin,[1])
+
+            fig.canvas.draw()
+            return None
+
+        # call the animator.  blit=True means only re-draw the parts that have changed.
+        anim = animation.FuncAnimation(fig, animate, frames=self.I.shape[0]-160,repeat=False)
+        
+        # save the animation as an mp4.  This requires ffmpeg or mencoder to be
+        # installed.  The extra_args ensure that the x264 codec is used, so that
+        # the video can be embedded in html5.  You may need to adjust this for
+        # your system: for more information, see
+        # http://matplotlib.sourceforge.net/api/animation_api.html
+
+        FFwriter = animation.FFMpegWriter()
+        anim.save('movie.mp4', writer=FFwriter,fps=15, extra_args=['-vcodec', 'libx264'])
+
+
+    def crosscorrelation(self, Nr=20, Nz=30, dr_max=0.05, dz_max=0.03, dkr_max=300, dkz_max=300, graph='2d'):
+        """
+        """
+        from scipy.signal import correlate2d
+        from mpl_toolkits.mplot3d import Axes3D
+
+        corr = np.zeros(2*np.array([Nr,Nz])-1)
+        
+        Igrid = self.interpolate(Nr,Nz,self.I)
+            
+        for i in range(self.Nt):
+            if np.isfinite(Igrid[i,...]).all():
+                corr += correlate2d(Igrid[i,...],Igrid[i,...])
+            else:
+                print 'miss'
+
+
+        corr /= np.max(corr)
+        temp = np.zeros(2*Nr-1)
+        r = self.r-self.r[0]
+        z = self.z-self.z[0]
+        temp[:Nr-1] = -r[Nr-1:0:-1]
+        temp[Nr-1:] = r
+        r = temp
+        temp = np.zeros(2*Nz-1)
+        temp[:Nz-1] = -z[Nz-1:0:-1]
+        temp[Nz-1:] = z
+        z = temp
+
+        indr_ = (r >= 0) & (r<dr_max)
+        indz_ = (z >= 0) & (z<dz_max)
+        ind = np.einsum('i,j->ij',indr_,indz_)
+        
+        rm, zm = np.meshgrid(r[indr_],z[indz_])
+        fft_corr = np.abs(np.fft.fft2(corr))/np.sqrt(Nr*Nz)
+        corr = corr[ind]
+        corr = np.reshape(corr,[np.sum(indr_),np.sum(indz_)])
+        
+        krfft = np.fft.fftfreq(Nr,r[2]-r[1])
+        kzfft = np.fft.fftfreq(Nz,z[2]-z[1])
+        indrfft = (krfft >= 0) & (krfft < dkr_max)
+        indzfft = (kzfft >= 0) & (kzfft < dkz_max)
+        krfft,kzfft = np.meshgrid(krfft[indrfft],kzfft[indzfft])
+        
+        fig = plt.figure()
+        plt.title('Correlation')
+        if graph == '3d':
+            ax = fig.gca(projection='3d')
+            surf = ax.plot_surface(rm,zm,corr.T, cmap=matplotlib.cm.coolwarm, rstride=2, linewidth=0, cstride=2)
+            fig.colorbar(surf)
+        else:
+            plt.contourf(r[indr_],z[indz_],corr.T,30)
+            plt.colorbar()
+        plt.xlabel('R')
+        plt.ylabel('Z')
+
+        fft_ = fft_corr[indrfft,:]
+        fft_ = fft_[:,indzfft]
+        
+        fig = plt.figure()
+        plt.title('FFT of the Correlation')
+        if graph == '3d':
+            ax = fig.gca(projection='3d')
+            surf = ax.plot_surface(krfft,kzfft,fft_.T, cmap=matplotlib.cm.coolwarm,rstride=1,linewidth=0, cstride=1)
+            fig.colorbar(surf)
+        else:
+            plt.contourf(krfft,kzfft,fft_,40)
+            plt.colorbar()
+        plt.xlabel('k_r')
+        plt.ylabel('k_z')
+            
+        plt.figure()
+        plt.plot(r[indr_],corr[:,0],label='R')
+        plt.plot(z[indz_],corr[0,:],label='Z')
+        plt.legend()
+        
+        plt.show()
+
             
     def computePSF(self, ne, Nr=20, Nz=30, ne_fluc=False):
         """ Compute the PSF
