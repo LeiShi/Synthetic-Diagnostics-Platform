b797a624395d1c6950409958c7df5049a2eb349f
shilei8583 <shilei8583@gmail.com>
2016-02-01 00:10:21 +0000

    Parameter1D has been added. Now function create_profile1D will return a 1D plasma profile.

--- src/FPSDP/Plasma/Analytical_Profiles/TestParameter.py
+++ src/FPSDP/Plasma/Analytical_Profiles/TestParameter.py
@@ -3,6 +3,7 @@
 
 
 """
+from collections import OrderedDict
 
 import numpy as np
 from numpy.random import random
@@ -15,11 +16,39 @@ from ..PlasmaProfile import ECEI_Profile
 # parameters.
 # can be modified via method set_parameter(kwargs).
 # Note that all the coordinates are in (Z,R) order.
-Parameter2D = { 'DownLeft':(-60,200), 'UpRight':(60,260),'NR':200, 'NZ':200, 
-               'ne_0': 2e13, 'Te_0': 1*cgs['keV'], 'B_0': 20000, 
-               'ne_shape': 'Hmode', 'Te_shape': 'Hmode', 'a': 50, 'R_0': 200,
-               'dne_ne':0.01, 'dte_te':0.01, 'dB_B':0.01, 
-               'timesteps':[0,1,2,3]}
+Parameter2D = OrderedDict()
+Parameter2D['R_0']=200
+Parameter2D['a']=50
+Parameter2D['DownLeft']=(-60,150)
+Parameter2D['UpRight']=(60,300)
+Parameter2D['NR']=201
+Parameter2D['NZ']=201
+Parameter2D['ne_0']=2e13
+Parameter2D['Te_0']=1*cgs['keV']
+Parameter2D['B_0']=20000
+Parameter2D['ne_shape']='Hmode'
+Parameter2D['Te_shape']='Hmode'
+Parameter2D['dne_ne']=0.01
+Parameter2D['dte_te']=0.01
+Parameter2D['dB_B']=0
+Parameter2D['timesteps']=[0]
+
+Parameter1D = OrderedDict()
+Parameter1D['R_0']=200
+Parameter1D['a']=50
+Parameter1D['Xmin']=150
+Parameter1D['Xmax']=300
+Parameter1D['NX']=201
+Parameter1D['ne_0']=2e13
+Parameter1D['Te_0']=1*cgs['keV']
+Parameter1D['B_0']=20000
+Parameter1D['ne_shape']='Hmode'
+Parameter1D['Te_shape']='Hmode'
+Parameter1D['dne_ne']=0.01
+Parameter1D['dte_te']=0.01
+Parameter1D['dB_B']=0
+Parameter1D['timesteps']=[0]
+               
 
 
 xgc_test2D ={'DownLeft':(-0.5,0.9),'UpRight':(0.5,1.6),'NR':101,'NZ':101}
@@ -32,6 +61,8 @@ xgc_test3D = {'Xmin':0.9,'Xmax':1.6,'Ymin':-0.5, 'Ymax':0.5, 'Zmin':-0.1,
 
 # shape table is a dictionary contains the shape parameters
 # Do not suggest to change it by outside programs
+# DecayScale means within a minor radius, it will decay to exponential of which
+# power.
 ShapeTable = {'exp': {'NeDecayScale': 3, 'TeDecayScale':5} , 
               'Hmode':{'PedWidthT': 0.02,'PedWidthN': 0.02 ,'PedHightT': 0.8, 
                        'PedHightN': 0.7, 'ne_out': 1e-10, 'Te_out': 1e-10}}
@@ -39,8 +70,14 @@ ShapeTable = {'exp': {'NeDecayScale': 3, 'TeDecayScale':5} ,
 def show_parameter2D():
     """Print out the parameters at the moment
     """
-    print Parameter2D.keys()
-    print Parameter2D
+    for key,value in Parameter2D.items():
+        print '{} : {}'.format(key, value)
+    
+def show_parameter1D():
+    """Print out the parameters at the moment
+    """
+    for key,value in Parameter1D.items():
+        print '{} : {}'.format(key, value) 
 
 def set_parameter2D(DownLeft=Parameter2D['DownLeft'], 
                     UpRight=Parameter2D['UpRight'], 
@@ -52,7 +89,11 @@ def set_parameter2D(DownLeft=Parameter2D['DownLeft'],
                     ne_shape=Parameter2D['ne_shape'], 
                     Te_shape=Parameter2D['Te_shape'], 
                     a=Parameter2D['a'], 
-                    R_0=Parameter2D['R_0']):
+                    R_0=Parameter2D['R_0'],
+                    dne_ne=Parameter2D['dne_ne'],
+                    dte_te=Parameter2D['dte_te'],
+                    dB_B=Parameter2D['dB_B'],
+                    timesteps=Parameter2D['timesteps']):
     """A explicit method to change the parameters.
 
     Although it's possible to directly assign new values to parameter keywords,
@@ -69,8 +110,130 @@ def set_parameter2D(DownLeft=Parameter2D['DownLeft'],
     Parameter2D['UpRight'] = UpRight
     Parameter2D['NR'] = NR
     Parameter2D['NZ'] = NZ
+    Parameter2D['dne_ne'] = dne_ne
+    Parameter2D['dte_te'] = dte_te
+    Parameter2D['dB_B'] = dB_B
+    Parameter2D['timesteps'] = timesteps
+    
+def set_parameter1D(Xmin=Parameter1D['Xmin'], 
+                    Xmax=Parameter1D['Xmax'], 
+                    NX=Parameter1D['NX'],
+                    ne_0=Parameter1D['ne_0'], 
+                    Te_0=Parameter1D['Te_0'], 
+                    B_0=Parameter1D['B_0'], 
+                    ne_shape=Parameter1D['ne_shape'], 
+                    Te_shape=Parameter1D['Te_shape'], 
+                    a=Parameter1D['a'], 
+                    R_0=Parameter1D['R_0'],
+                    dne_ne=Parameter1D['dne_ne'],
+                    dte_te=Parameter1D['dte_te'],
+                    dB_B=Parameter1D['dB_B'],
+                    timesteps=Parameter1D['timesteps']):
+    """A explicit method to change the parameters.
 
-def create_profile2D(random_fluctuation=False):
+    Although it's possible to directly assign new values to parameter keywords,
+    it may be more comfortable for C programmers to do it this way.
+    """
+    Parameter1D['ne_0'] = ne_0
+    Parameter1D['Te_0'] = Te_0
+    Parameter1D['B_0'] = B_0
+    Parameter1D['a'] = a
+    Parameter1D['R_0'] = R_0
+    Parameter1D['ne_shape'] = ne_shape
+    Parameter1D['Te_shape'] = Te_shape
+    Parameter1D['Xmin'] = Xmin
+    Parameter1D['Xmax'] = Xmax
+    Parameter1D['NX'] = NX
+    Parameter1D['dne_ne'] = dne_ne
+    Parameter1D['dte_te'] = dte_te
+    Parameter1D['dB_B'] = dB_B
+    Parameter1D['timesteps'] = timesteps
+
+def create_profile1D(random_fluctuation=False, fluc_level=None):
+    """Create the profiles and return it in a dictionary structure
+
+    ne, Te, B values on RZ mesh are returned
+    """
+
+    # the return value will be generated by this dictionary
+    profile = {}
+
+    # extract the plasma shape information
+    a= Parameter1D['a']
+    R_0= Parameter1D['R_0']
+    XGrid = Grid.Cartesian1D(Parameter1D['Xmin'], Parameter1D['Xmax'], 
+                             Parameter1D['NX'])
+
+    profile['Grid'] = XGrid
+
+    # extract the density information
+    nshp = Parameter1D['ne_shape']
+    ne_0 = Parameter1D['ne_0']
+
+
+    # evaluate the density values on each grid point for the given shape
+    if (nshp == 'exp') :
+        # exponential decay shape
+        DecScale= ShapeTable['exp']['NeDecayScale']
+        DecLength= a/DecScale
+        ne_array= ne_0 * np.exp(-np.abs(XGrid.X1D-R_0)/DecLength )
+    elif (nshp == 'Hmode') :
+        # linear H mode profile
+        nped = ne_0 * ShapeTable['Hmode']['PedHightN']
+        nout = ne_0 * ShapeTable['Hmode']['ne_out']
+        a_core = a * (1-ShapeTable['Hmode']['PedWidthN'])
+        a_array = np.abs(XGrid.X1D - R_0)
+        # linear function connecting axis, top of pedestal and the vacuum
+        ne_array = np.select([a_array <= a_core, a_array >= a, 
+                              a_array > a_core ], 
+                             [a_array* (nped - ne_0)/a_core + ne_0 , nout,  
+                              (a_array - a) * (nped-nout)/(a_core - a)+ nout])
+    profile['ne']= ne_array
+
+    # Te profile
+    tshp = Parameter1D['Te_shape']
+    Te_0 = Parameter1D['Te_0']
+
+    if ( tshp == 'exp'):
+        DecLength= a/ShapeTable['exp']['TeDecayScale']
+        Te_array= Te_0 * np.exp(-np.abs(XGrid.X1D-R_0)/DecLength )
+    elif (tshp == 'Hmode') :
+        # linear H mode profile
+        tped = Te_0 * ShapeTable['Hmode']['PedHightT']
+        tout = Te_0 * ShapeTable['Hmode']['Te_out']
+        a_core = a * (1-ShapeTable['Hmode']['PedWidthT'])
+        a_array = np.abs(XGrid.X1D - R_0)
+        # linear function connecting axis, top of pedestal and the vacuum
+        Te_array = np.select([a_array<=a_core, a_array>=a, a_array>a_core],
+                             [a_array*(tped-Te_0)/a_core + Te_0, tout,
+                              (a_array-a)*(tped-tout)/(a_core-a)+ tout])
+    profile['Te']= Te_array
+
+    # B profile
+    B_0 = Parameter1D['B_0']
+    B_array= B_0 * R_0/XGrid.X1D
+    profile['B']= B_array
+    
+    
+    if random_fluctuation:
+        
+        time = Parameter1D['timesteps']
+        d_shape = [len(time), XGrid.shape[0]]
+        if fluc_level is None:
+            dne = 2*Parameter1D['dne_ne']*profile['ne']*(random(d_shape)-0.5)
+            dTe = 2*Parameter1D['dTe_Te']*profile['Te']*(random(d_shape)-0.5)
+            dB = 2*Parameter1D['dB_B']*profile['B']*(random(d_shape)-0.5)
+        else:
+            dne = 2*fluc_level*profile['ne']*(random(d_shape)-0.5)
+            dTe = 2*fluc_level*profile['Te']*(random(d_shape)-0.5)
+            dB = 2*fluc_level*profile['B']*(random(d_shape)-0.5)
+        return ECEI_Profile(profile['Grid'],profile['ne'],profile['Te'],
+                        profile['B'], time, dne, dTe, dTe, dB)
+    else:
+        return ECEI_Profile(profile['Grid'],profile['ne'],profile['Te'],
+                        profile['B'])
+
+def create_profile2D(random_fluctuation=False, fluc_level=None):
     """Create the profiles and return it in a dictionary structure
 
     ne, Te, B values on RZ mesh are returned
@@ -141,9 +304,14 @@ def create_profile2D(random_fluctuation=False):
         time = Parameter2D['timesteps']
         d_shape = [len(time)]
         d_shape.extend([i for i in RZGrid.R2D.shape])
-        dne = 2*Parameter2D['dne_ne']*profile['ne']*random(d_shape)
-        dTe = 2*Parameter2D['dne_ne']*profile['Te']*random(d_shape)
-        dB = 2*Parameter2D['dB_B']*profile['B']*random(d_shape)
+        if fluc_level is None:
+            dne = 2*Parameter2D['dne_ne']*profile['ne']*(random(d_shape)-0.5)
+            dTe = 2*Parameter2D['dte_te']*profile['Te']*(random(d_shape)-0.5)
+            dB = 2*Parameter2D['dB_B']*profile['B']*(random(d_shape)-0.5)
+        else:
+            dne = 2*fluc_level*profile['ne']*(random(d_shape)-0.5)
+            dTe = 2*fluc_level*profile['Te']*(random(d_shape)-0.5)
+            dB = 2*fluc_level*profile['B']*(random(d_shape)-0.5)
         return ECEI_Profile(profile['Grid'],profile['ne'],profile['Te'],
                         profile['B'], time, dne, dTe, dTe, dB)
     else:
