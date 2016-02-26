d12428199b782d0adbb0426f7d450ae15522acce
Lei Shi <shilei8583@gmail.com>
2016-02-25 22:30:11 -0500

    Style Update

--- src/FPSDP/Diagnostics/Reflectometry/FWR2D/Postprocess.py
+++ src/FPSDP/Diagnostics/Reflectometry/FWR2D/Postprocess.py
@@ -9,34 +9,44 @@ Advanced Postprocesses:
     Correlation reflectometry analysis,
     Frequency spectrum analysis,
 """
-from ....IO import Code5 as c5
+from os import path
 
 import numpy as np
 import scipy.io.netcdf as nc
 from scipy.optimize import curve_fit
-from scipy.integrate import dblquad
 from scipy.interpolate import interp2d
 
-from ..analysis import phase
+from ....IO import Code5 as c5
 
 
 class Reflectometer_Output:
-    """Class that deal with the raw output from synthetic reflectometry code(FWR2D / FWR3D)
+    """Class that deal with the raw output from synthetic reflectometry 
+    code(FWR2D / FWR3D)
 
-    Initialize with a output path, a frequency array , and a time sequence array
-    
+    :param string file_path: Path to FWR output files
+    :param f_arr: frequency channels to be loaded
+    :type f_arr: 1D array of float
+    :param t_arr: simulation time steps to be loaded
+    :type t_arr: 1D array of int
+    :param int n_cross_section: total number of cross-sections used
+    :param int FWR_dimension: either 2 or 3, default is 2.
+    :param bool full_load: Optional, Default is True. 
+                           If True, create reflected signal from FWR run output
+                           files, if False, skip the creating part, wait for 
+                           loading a saved data file.
+    :param string receiver_file_name: file name for the Code5 file specifying 
+                                      the receiver's electric field. Optional,
+                                      default is the default value set in 
+                                      FWR_Driver script, this is the receiver 
+                                      field pattern link name in every single 
+                                      run dir created by FWR_Driver.
     """
 
+    
     def __init__(self,file_path,f_arr,t_arr,n_cross_section, FWR_dimension = 2,full_load = True,receiver_file_name='receiver_pattern.txt'):
-        """initialize the output object, read the output files in file_path specified by frequencies and timesteps.
-        Arguments:
-            file_path: string, absolute or relative path to the the directory containing frequency level dirs. This should be the same as the 'full_output_path' in FWR2D_Driver script used to initiate the Runs.
-            f_arr: list of double, frequency array contained in this run
-            t_arr: list of int, time step array contained in this run
-            n_cross_section: int, total number of cross_sctions used in this run
-            FWR_dimension: optional int, either 2 or 3, default 2, give the dimension of FWR run, to use corresponding signal collecting function
-            full_load: optional bool, default True. if True, create reflected signal from FWR run output files, if False, skip the creating part, wait for loading a saved data file.
-            receiver_file_name:optional string, default to be the default value set in FWR_Driver script, this is the receiver field pattern link name in every single run dir created by FWR_Driver.
+        """initialize the output object, read the output files in file_path 
+        specified by frequencies and timesteps.
+        
         """
         self.file_path = file_path
         self.frequencies = f_arr
@@ -49,10 +59,13 @@ class Reflectometer_Output:
         if (full_load):
             self.create_received_signals()
         else:
-            print 'Initializer didn\'t creat E_out. Need to read E_out from somewhere else.' 
+            print('Initializer didn\'t creat E_out. Need to read E_out from \
+somewhere else.') 
+    
     
     def create_received_signals(self):
-        """This method actually recursively read all the needed output files and produce the recieved signal for each file.
+        """This method actually recursively read all the needed output files 
+        and produce the recieved signal for each file.
         """
         self.E_out = np.zeros((self.NF,self.NT,self.n_cross_section))
         self.E_out = self.E_out + 1j * self.E_out
@@ -60,35 +73,46 @@ class Reflectometer_Output:
         for f_idx in range(self.NF):
             for t_idx in range(self.NT):
                 for i in range(self.n_cross_section):
-                    fwr_file = self.make_file_name(self.frequencies[f_idx],self.timesteps[t_idx],i)
-                    receiver_file = self.make_receiver_file_name(self.frequencies[f_idx],self.timesteps[t_idx],i,self.receiver_file_name)
-                    self.E_out[f_idx,t_idx,i] = self.read_E_out(fwr_file,receiver_file)
-            print 'frequency '+str(self.frequencies[f_idx])+' read.'
+                    fwr_file = self.make_file_name(self.frequencies[f_idx],
+                                                   self.timesteps[t_idx], i)
+                    receiver_file = \
+                    self.make_receiver_file_name(self.frequencies[f_idx],
+                                                 self.timesteps[t_idx], i, 
+                                                 self.receiver_file_name)
+                                                 
+                    self.E_out[f_idx,t_idx,i] = self.read_E_out(fwr_file,
+                                                                receiver_file)
+            print('frequency '+str(self.frequencies[f_idx])+' read.')
                 
         return 0
     
 
     def make_file_name(self,f,t,nc):
-        """create the corresponding output file name based on the given frequency and time
+        """create the corresponding output file name based on the given 
+        frequency and time
         """
-        full_path = self.file_path + str(f)+'/'+str(t)+'/'+str(nc)+'/'
+        full_path = path.join(self.file_path, str(f), str(t), str(nc) )
         
         if self.dimension ==2:
             file_name = 'out_{0:0>6.2f}_equ.cdf'.format(f)
         elif self.dimension == 3:
             file_name = 'schradi.cdf'
             
-        return full_path + file_name
+        return path.join(full_path ,file_name)
 
-    def make_receiver_file_name(self,f,t,nc,file_name):
-        """create the receiver antenna pattern file name. Rightnow, it works with the NSTX_FWR_Driver.py script default.
+    
+    def make_receiver_file_name(self, f, t, nc, file_name):
+        """create the receiver antenna pattern file name. 
+        
+        Rightnow, it works with the NSTX_FWR_Driver.py script default.
         """
-        full_path = '{0}{1}/{2}/{3}/'.format(self.file_path,str(f),str(t),str(nc))
-        return full_path + file_name
+        full_path = path.join(self.file_path, str(f), str(t), str(nc))
+        return path.join(full_path, file_name)
     
 
-    def read_E_out(self,ref_file,rec_file):
-        """read data from the output file, produce the received E signal, return the complex E
+    def read_E_out(self, ref_file, rec_file):
+        """read data from the output file, produce the received E signal, 
+        return the complex E
         """
         #print 'reading file:',file
         f = nc.netcdf_file(ref_file,'r')
@@ -99,11 +123,14 @@ class Reflectometer_Output:
             z = np.copy(f.variables['a_z'].data)
             z_idx = f.dimensions['a_nz']/2 -1
             
-            #FWR2D output doesn't include the central ray WKB phase advance in paraxial region. The complete result needs to /
-            #take into account this additional phase
-            ephi_wkb = f.variables['p_phaser'][...] + 1j * f.variables['p_phasei'][...]
+            # FWR2D output doesn't include the central ray WKB phase advance in
+            # paraxial region. The complete result needs to take into account 
+            # this additional phase
+            ephi_wkb = f.variables['p_phaser'][...] +\
+                       1j * f.variables['p_phasei'][...]
             D_ephi = ephi_wkb[0]/ephi_wkb[-1]
-            E_ref = np.copy(f.variables['a_Er'][1,z_idx,:] + 1j*f.variables['a_Ei'][1,z_idx,:])*D_ephi**2
+            E_ref = np.copy(f.variables['a_Er'][1,z_idx,:] + \
+                            1j*f.variables['a_Ei'][1,z_idx,:])*D_ephi**2
             f.close()
 
             receiver = c5.C5_reader(rec_file)
@@ -112,52 +139,63 @@ class Reflectometer_Output:
             
             E_out = np.trapz(E_ref*np.conj(E_rec[z_idx,:]),x=y)
             return E_out
+            
         elif(self.dimension == 3):
             y = np.copy(f.variables['a_y'][:])
             z = np.copy(f.variables['a_z'][:])
             
-            E_ref_re_interp = interp2d(y,z,f.variables['a_Er'][1,:,:],kind='cubic',fill_value=0)
-            E_ref_im_interp = interp2d(y,z,f.variables['a_Ei'][1,:,:],kind='cubic',fill_value=0)            
+            E_ref_re_interp = interp2d(y,z,f.variables['a_Er'][1,:,:],
+                                       kind='cubic', fill_value=0)
+            E_ref_im_interp = interp2d(y,z,f.variables['a_Ei'][1,:,:],
+                                       kind='cubic', fill_value=0)            
             f.close()
 
             receiver = c5.C5_reader(rec_file)
             
             #use area average to estimate E_ref*np.conj(E_receive) integrated over y,z dimension.
-            ymin = np.max([y[0],receiver.X1D[0]])
-            ymax = np.min([y[-1],receiver.X1D[-1]])
-            zmin = np.max([z[0],receiver.Y1D[0]])
-            zmax = np.min([z[-1],receiver.Y1D[-1]])
-            y_fine = np.linspace(ymin,ymax,200)
-            z_fine = np.linspace(zmin,zmax,200)
-            self.E_ref = E_ref_re_interp(y_fine,z_fine)+ 1j*E_ref_im_interp(y_fine,z_fine)
-            self.E_rec = receiver.E_re_interp(z_fine,y_fine)+ 1j*receiver.E_im_interp(z_fine,y_fine)
+            ymin = np.max([y[0], receiver.X1D[0]])
+            ymax = np.min([y[-1], receiver.X1D[-1]])
+            zmin = np.max([z[0], receiver.Y1D[0]])
+            zmax = np.min([z[-1], receiver.Y1D[-1]])
+            y_fine = np.linspace(ymin, ymax, 200)
+            z_fine = np.linspace(zmin, zmax, 200)
+            self.E_ref = E_ref_re_interp(y_fine, z_fine)+ \
+                         1j*E_ref_im_interp(y_fine, z_fine)
+            self.E_rec = receiver.E_re_interp(z_fine,y_fine)+ \
+                         1j*receiver.E_im_interp(z_fine,y_fine)
             
             E_conv = self.E_ref*np.conj(self.E_rec)
 
-            dy = y_fine[1]-y_fine[0]
-            dz = z_fine[1]-z_fine[0]
+            dy = y_fine[1] - y_fine[0]
+            dz = z_fine[1] - z_fine[0]
             
-            SEy = np.sum(E_conv[:,:-1]+E_conv[:,1:],axis = 1)/2*dy #integration over y direction
-            E_out = np.sum(SEy[:-1]+SEy[1:])/2*dz #integration over z direction
+            # integration over y direction
+            SEy = np.sum(E_conv[:,:-1] + E_conv[:,1:], axis=1)/2*dy 
+            # integration over z direction
+            E_out = np.sum(SEy[:-1] + SEy[1:])/2*dz 
             return E_out
 
-    def save_E_out(self,filename = 'E_out.sav'):
+    
+    def save_E_out(self, filename='E_out.sav'):
         """Save the output signal array to a binary file
         Default filename is 'E_out.sav.npy'
         Note that a '.npy' extension will be added automatically
         """
-        np.save(self.file_path + filename,self.E_out)
+        np.save(path.join(self.file_path,filename), self.E_out)
 
-    def load_E_out(self,filename = 'E_out.sav'):
+    
+    def load_E_out(self, filename='E_out.sav'):
         """load an existing E_out array from previously saved datafile.
         Default filename is E_out.sav.npy
         Note that a '.npy' extension will be automatically added if not given in filename.
         """
         if('.npy' not in filename):
-            filename = filename+'.npy'
-        self.E_out =  np.load(self.file_path + filename)
+            filename = filename + '.npy'
+        self.E_out =  np.load(path.join(self.file_path, filename))
         
         
+
+#TODO Check the status of these funcions and delete deprecated ones.
 
 def Self_Correlation(ref_output,tstart=None, tend=None):
     """Calculate the self correlation function for each frequency
@@ -170,7 +208,7 @@ def Self_Correlation(ref_output,tstart=None, tend=None):
 
     input: Reflectometer_Output object
     """
-    if( tstartis None and tend is None):
+    if( tstart is None and tend is None):
         M = ref_output.E_out
     elif(tstart is None):
         M = ref_output.E_out[:,:tend+1,:]
@@ -184,6 +222,7 @@ def Self_Correlation(ref_output,tstart=None, tend=None):
     M2_bar = np.average(np.average(M*np.conj(M),axis = 2),axis = 1)
 
     return M_bar/np.sqrt(M2_bar)
+
 
 def Cross_Correlation(ref_output):
     """Calculate the cross correlation between different frequency channels
@@ -218,6 +257,7 @@ def Cross_Correlation(ref_output):
                 pass
 
     return r
+
 
 def Cross_Correlation_by_fft(ref_output):
     """Calculate teh cross correlation using fft method. Details can be found in Appendix part of ref.[1]
@@ -264,9 +304,11 @@ def gaussian_fit(x,a):
 
     return np.exp(-x**2/a)
 
+
 def exponential_fit(x,a):
 
     return np.exp(-np.abs(x)/a)
+
 
 def fitting_cross_correlation(cross_cor_arr,dx_arr,fitting_type = 'gaussian'):
     """ fit the cross_correlation points with chosen function, default to be gaussian.
@@ -294,11 +336,13 @@ def fitting_cross_correlation(cross_cor_arr,dx_arr,fitting_type = 'gaussian'):
 
     return (a,np.sqrt(sigma_a2))
     
+
 def remove_average_field(E_ref):
     """calculate the perturbed reflected signal by removing average signal for each cross section.
     """    
     E =E_ref - np.mean(E_ref,axis = 1,keepdims = True)
     return E
+
 
 def average_phase(sig):
     """ calculate the averaged phase of a series of complex signals. The phase for each signal is determined as follows:
@@ -319,6 +363,7 @@ def average_phase(sig):
         avg_phase = np.average([avg_phase,ph_chosen],weights=[i,1])
         i+=1
     return avg_phase    
+
 
 def remove_average_phase(E_ref):
     """ Rotate the whole reflected signal from each cross section clockwisely by the amount of their averaged phase, so the resulted signals have zero mean phase.
