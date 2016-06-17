7033190ded86538f72837cbea0ae9f669d85073f
Lei Shi <shilei8583@gmail.com>
2016-06-17 17:44:56 -0400

    setup.py now searches the whole src/ path for packages.

--- setup.py
+++ setup.py
@@ -10,20 +10,25 @@ Created on Thu Jun 16 17:38:55 2016
 from setuptools import setup, find_packages
 
 setup(name='sdp',
-      version='0.1.0',
+
+	  # use git tag as the version number
+      use_scm_version = True,
+	  
       description='Synthetic Diagnostics Platform for Fusion Plasmas',
       author='Lei Shi',
       author_email='FPSDP.main@gmail.com',
       url='https://github.com/LeiShi/Fusion_Plasma_Synthetic_Diagnostics_\
 Platform-Public-',
-      packages = ['sdp'], #find_packages(),
+      packages = find_packages('./src'),
 	  package_dir = {'sdp':'src/sdp'},
-      include_package_data = True,
-      package_data= {'sdp.math' : ['data/*']},
-	  data_files = [('', ['./LICENSE', './AUTHORS', './NOTICE'])],
+      #include_package_data = True,
+      package_data= {'sdp.math' : ['data/*'],
+					 'sdp' : ['./LICENSE', './AUTHORS', './NOTICE']},
+	  #data_files = [('src/sdp', ['./LICENSE', './AUTHORS', './NOTICE'])],
       license = 'revised-BSD',
       
       # dependencies
-      install_requires=['numpy>=1.10.4', 'scipy>=0.17.0']
+      install_requires=['numpy>=1.10.4', 'scipy>=0.17.0'],
+	  setup_requires = ['setuptools_scm', 'setuptools_scm_git_archive']
       
 )
