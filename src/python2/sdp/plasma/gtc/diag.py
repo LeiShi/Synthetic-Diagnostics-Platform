135ffd9f4387bbbfbc2b10dcdb4ba0dfc9a39bc7
Lei Shi <shilei8583@gmail.com>
2017-07-13 10:23:10 -0700

    GTC RMP Alpha spline diagnostic
    GTC calculated alpha spline coefficients are outputed in SDP output
    files. The SDP gtc loader suite is updated to read and analyse this
    data.

--- src/python2/sdp/plasma/gtc/diag.py
+++ src/python2/sdp/plasma/gtc/diag.py
@@ -1,4 +1,5 @@
 import json
+import scipy.io.netcdf as nc
 import numpy as np
 
 class Diagnoser(object):
@@ -25,12 +26,7 @@ class Diagnoser(object):
                                        reshape(( self.ntheta, self.npsi, 9) )
         self._dpsi = 1./(self.npsi-1)
         self._dtheta = np.pi*2/(self.ntheta-1)
-        #TODO change this part to use read-in psi_separatrix
-        try:
-            self._psi_separatrix = raw_diag['psiw']
-        except KeyError:
-            print 'old diag file, use preset psi_separatrix'
-            self._psi_separatrix = 5.1879e-2
+        self._psi_separatrix = raw_diag['psi_separatrix']
 
     def _sp1d(self, psi, ysp):
         n = np.floor(psi/self._dpsi).astype(np.int)
@@ -79,3 +75,90 @@ boundary. Check psi values, they should be normalized psi_wall.')
     def jb_sp(self, psi, theta):
         return self._sp2d(psi, theta, self.jacobian_boozer)
 
+class AlphaDiagnoser(object):
+    """ Diagnoser for magnetic perturbations in GTC obtained from M3DC1"""
+
+    def __init__(self, filename='alpha_sdp.nc'):
+        raw_alpha = nc.netcdf_file(filename, 'r')
+        self.npsi = raw_alpha.dimensions['npsi']
+        self.ntheta = raw_alpha.dimensions['ntheta']
+        self.nzeta = raw_alpha.dimensions['nzeta']
+        self._spdim = raw_alpha.dimensions['spdim']
+        self._psi_separatrix = raw_alpha.variables['psi_separatrix'].data
+        self._raw_alpha = raw_alpha.variables['alpha'].data
+        assert self._raw_alpha.shape==(self.nzeta, self.ntheta, self.npsi,
+                                         self._spdim)
+        assert self._spdim==27, \
+            "Quadratic spline is assumed. Other type not implemented"
+        # calculate the step sizes in all dimensions
+        self._dpsi = 1./(self.npsi-1)
+        self._dtheta = np.pi*2/(self.ntheta-1)
+        self._dzeta = np.pi*2/(self.nzeta-1)
+
+    def _sp3d(self, psi, theta, zeta, ysp):
+        if(np.any(psi>1)):
+            raise ValueError('psi value greater than 1, outside of plasma \
+boundary. Check psi values, they should be normalized psi_wall.')
+        theta = np.remainder(theta, 2*np.pi)
+        zeta = np.remainder(zeta, 2*np.pi)
+
+        # obtain index and remainder in psi
+        # note that the GTC spline in done on psi in GTC unit, not normalized
+        # to psiw
+        i = np.floor(psi/self._dpsi).astype(np.int)
+        psi_r = psi - i*self._dpsi
+        psi_re = psi_r*self._psi_separatrix
+        # special care is needed for i==0 grid, the spline is done with respect
+        # to sqrt(psi) in this cell
+        psi_re = np.where(i==0, np.sqrt(psi_re), psi_re)
+
+        # obtain index and remainder in theta
+        j = np.floor(theta/self._dtheta).astype(np.int)
+        theta_re= theta - j*self._dtheta
+
+        # obtain index and remainder in theta
+        k = np.floor(zeta/self._dzeta).astype(np.int)
+        zeta_re= zeta - k*self._dzeta
+
+        # create the 3D spline vector
+        dx_vec = np.transpose([psi_re**p*theta_re**n*zeta_re**m for m in range(3)
+                           for n in range(3) for p in range(3)], (1,2,3,0))
+
+        return np.sum(ysp[k,j,i,:]*dx_vec, axis=-1)
+
+    def alpha_sp(self, psi, theta, zeta):
+        """evaluate alpha using GTC spline coefficients"""
+        return self._sp3d(psi, theta, zeta, self._raw_alpha)
+
+    def fourier_analysis(self, psi, ntheta, nzeta):
+        """ Evaluate alpha_mn(psi) based on the GTC alpha spline
+
+        It is important to make sure the Fourier transformation convention used
+        in GTC and M3DC1 is the same as here. Check the convention in GTC
+        documentations.
+        """
+        psi_1d = np.array(psi)
+        assert psi_1d.ndim <= 1, "Only 1D array of psi is allowed."
+        assert nzeta%2==1, 'nzeta need to be odd for FFT shifts'
+        theta_1d = np.linspace(0, 2*np.pi, ntheta)
+        zeta_1d = np.linspace(0, 2*np.pi, nzeta)
+
+        zeta_mesh, theta_mesh, psi_mesh = np.meshgrid(zeta_1d, theta_1d,
+                                                      psi_1d, indexing='ij')
+        # spline interpolate the alpha values on 3D mesh
+        alpha_arr = self._sp3d(psi_mesh, theta_mesh, zeta_mesh, self._raw_alpha)
+        # Calculate the Harmonics
+        # Note that the convention was alpha = sum alpha_mn*exp(in zeta-im theta)
+        # So the inversed relation is alpha_mn = 1/mn sum alpha*exp(-in zeta +
+        # im theta)
+        alpha_n = np.fft.fft(alpha_arr,axis=0,norm=None)/nzeta
+        # the np.fft.fft convention has the opposite sign for zeta, we need to
+        # revert the n harmonics, first shift the array to have increasing
+        # order
+        alpha_n = np.fft.fftshift(alpha_n, axes=0)
+        # revert the whole array
+        alpha_n = np.flip(alpha_n, axis=0)
+        # shift back to FFT order, nzeta is assumed odd
+        alpha_n = np.fft.ifftshift(alpha_n, axes=0)
+        # normal FFT on theta and return
+        return np.fft.fft(alpha_n, axis=1,norm=None)/ntheta
